a6232a817b60d41ef2263199a7f5497c2c2d2762
Kristina <ccthw@protonmail.com>
2020-07-08 12:09:16 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MLAB_URI || 
+    `mongodb+srv://${process.env.MONGO_NAME}:${process.env.MONGO_KEY}@ks-ujl29.mongodb.net/exercise-tracker?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+// 404 Error middleware
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err); 
+});
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    let errCode, errMessage;
+    if (err.errors) {
+        // Mongoose validation error
+        errCode = 400 // bad request
+        const keys = Object.keys(err.errors);
+        // Report the first validation error
+        errMessage = err.errors[keys[0]].message;
+    } else {
+        // Generic or custom error
+        errCode = err.status || 500;
+        errMessage = err.message || 'Internal Server Error';
+    }
+    res.status(errCode).json({ error: errMessage });
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Express is listening on port ' + listener.address().port);
+});
