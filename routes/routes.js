bcd0791461ff46f453c5d20c114b1f1738da8861
Kristina <ccthw@protonmail.com>
2020-07-09 15:54:40 +0300

    Build routes

--- routes/routes.js
+++ routes/routes.js
@@ -37,18 +37,20 @@ router.post('/exercise/add', (req, res, next) => {
     if (req.body.date === '') req.body.date = undefined;  
     User.findById(req.body.userId, (err, user) => {
         if (err) return next(err);
-        Exercise.create(req.body, (err, exercise) => {
-            if (err) return next(err); 
-            res.json({
-                userId: user._id,
-                username: user.username,
-                exercise: {
-                    description: exercise.description,
-                    duration: exercise.duration, 
-                    date: exercise.date   
-                }
+        if (user) {
+            Exercise.create(req.body, (err, exercise) => {
+                if (err) return next(err); 
+                res.json({
+                    userId: user._id,
+                    username: user.username,
+                    exercise: {
+                        description: exercise.description,
+                        duration: exercise.duration, 
+                        date: exercise.date   
+                    }
+                });
             });
-        });
+        }
     });
 });
 
@@ -56,11 +58,59 @@ router.post('/exercise/add', (req, res, next) => {
  * GET '/api/exercise/log/:user_id'
  * Return the user object with a full array of exercise log and total exercise count - 200  
  */
+router.get('/exercise/log/:user_id', (req, res, next) => {
+    User.findById(req.params.user_id, (err, user) => {
+        if (err) next(err);
+        if (user) {
+            Exercise.find({ userId: user._id }, (err, exercises) => {
+                if (err) next(err);
+                res.json({
+                    userId: user._id,
+                    username: user.username,
+                    exerciseCount: exercises.length,  
+                    exercises
+                });
+            });
+        }
+    });
+});
 
 /**
  * GET '/api/exercise/log/:user_id:from:to:limit'
  * Retrieve any part of the exercise log for any user - 200 
  * Date format yyyy-mm-dd, limit - int
  */
+router.get('/exercise/log/:user_id/:from?/:to?/:limit?', (req, res, next) => {
+    const { user_id, from, to, limit } = req.params;
+    User.findById(user_id, (err, user) => {
+        if (err) next(err);
+        if (user) {
+            const { _id, username } = user;
+            if (from && to && limit) {
+                Exercise.find({ 
+                    userId: _id, 
+                    date: { $gte: from, $lte: to }, 
+                    duration: { $lte: limit } 
+                }, (err, exercises) => {
+                    if (err) next(err);
+                    res.json({ userId: _id, username, exercises });
+                });
+            } else if (from && to) {
+                Exercise.find({ 
+                    userId: _id, 
+                    date: { $gte: from, $lte: to }
+                }, (err, exercises) => {
+                    if (err) next(err);
+                    res.json({ userId: _id, username, exercises });
+                });
+            } else if (from) {
+                Exercise.find({ userId: _id, date: { $gte: from }}, (err, exercises) => {
+                    if (err) next(err);
+                    res.json({ userId: _id, username, exercises });
+                });
+            }
+        }
+    });
+});
 
 module.exports = router; 
